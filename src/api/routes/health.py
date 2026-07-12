b2744d306280876e76fa027b03cfbda31217bd84
santana98 <eliasvssantana@gmail.com>
2026-07-12 08:45:05 -0300

    lint: ruff check && format

--- src/api/routes/health.py
+++ src/api/routes/health.py
@@ -1,8 +1,10 @@
 from http import HTTPStatus
 from fastapi import APIRouter
 
+
 router = APIRouter()
 
-@router.get('/health', status_code=HTTPStatus.OK)
+
+@router.get("/health", status_code=HTTPStatus.OK)
 def health():
     return {"status": "ok"}
--- src/api/routes/recommendations.py
+++ src/api/routes/recommendations.py
@@ -1,24 +1,19 @@
-# from http import HTTPStatus
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 
 from src.core.schemas import ResponseRecommendations
 
 router = APIRouter()
 
-@router.get('/recommendations/{user_id}')
-def recommendations(user_id: str):
-    return ResponseRecommendations(
+
+@router.get("/recommendations/{user_id}")
+def recommendations(user_id: str, request: Request):
+
+    user_recommendations = ResponseRecommendations(
         user_id=user_id,
         products_recommendations=[
-            {
-                "product": "0001",
-                "purchase_propensity_score": 0.8,
-                "affinity_user": 1
-            },
-            {
-                "product": "0002",
-                "purchase_propensity_score": 0.4,
-                "affinity_user": 0
-            }
-        ]
+            {"product": "0001", "purchase_propensity_score": 0.8, "affinity_user": 1},
+            {"product": "0002", "purchase_propensity_score": 0.4, "affinity_user": 0},
+        ],
     )
+
+    return user_recommendations
